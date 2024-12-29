b3cd854b2bd8865e90de71facea9f83811635ce1
linmo <hc0708king@163.com>
2024-12-29 20:08:31 +0800

    导航条部分数据传递

--- project/lc/js/base.js
+++ project/lc/js/base.js
@@ -1,32 +1,125 @@
-let shopNav = document.querySelector('.shop-subnav')
-let shopli = shopNav.querySelectorAll('ul li')
-let shopliChild = shopNav.querySelectorAll('ul .listChild')
-console.log(shopNav);
-console.log(shopli);
-console.log(shopliChild);
+const BASE_URL = 'http://110.41.43.118:3008';
 
-for (let i = 0; i < shopli.length; i++) {
-    if (i >= 0 && i <= 6) {
-        shopli[i].onmouseenter = function () {
-            shopliChild[0].style.display = 'block'
+//获取元素
+//导航栏商店子菜单部分
+let shopNav = document.querySelector('.shop-subnav');
+let shopUl = shopNav.querySelector('ul');
+let shopLi = shopUl.getElementsByClassName('cat');//所有导航分类
+//搜索部分
+let search = document.querySelector('.search-content')
+let searchInput = search.querySelector('input')
+let searchSpan = search.querySelector('span')
+console.log(searchInput);
+
+
+
+
+//请求元素全局变量
+let catData = {};//商品导航
+let classifyData = {};//商品子导航
+
+//请求商品分类导航的数据
+(function () {
+    hc_ajax.ajax({
+        method: 'get',
+        url: BASE_URL + '/api_cat',
+        success(res) {
+            console.log(res);
+            // 验证请求结果
+            if (res.code != 0) {
+                console.log(res);
+                return;
+            };
+
+            //想要的结果
+            catData = res.data;
+
+            //遍历操作
+            let str = '';
+            catData.forEach(item => {
+                // 累加数据
+                str += `
+                    <li class='cat'>
+                        <a href="">
+                            <img src="${item.cat_img}" alt="" />
+                            <p>${item.cat_name}</p>
+                        </a>
+                    </li>
+                `;
+            });
+            // 添加到对应的结构
+            shopUl.innerHTML = str;
         }
-        shopli[i].onmouseleave = function () {
-             shopliChild[0].style.display = 'none'
+    });
+})();
+
+//请求商品分类导航的小分类数据
+(function () {
+    hc_ajax.ajax({
+        method: 'get',
+        url: BASE_URL + '/api_classify',
+        success(res) {
+            console.log(res);
+            // 数据
+            classifyData = res.data;
+            //插入小分类的li,每七行的小分类在一起，层叠且默认隐藏
+            let listChild = document.createElement('li');
+            listChild.className = 'listChild';
+            let children = shopUl.children;
+            //插入子导航合集
+            shopUl.insertBefore(listChild.cloneNode(true), children[7]);
+            shopUl.insertBefore(listChild.cloneNode(true), children[15]);
+            shopUl.insertBefore(listChild.cloneNode(true), children[17]);
+            //获取所有子导航合集
+            let shopliChild = shopNav.querySelectorAll('ul .listChild');
+            
+            // 思路：鼠标移入对应菜单栏的时候，对catID进行匹配，再将数据放进对应listChild里面
+            //遍历所有导航分类
+            for (let index = 0; index < shopLi.length; index++) {
+                //鼠标移入导航栏
+                shopLi[index].onmouseenter = function(){
+                    //遍历子导航数据
+                    for (let i = 0; i < classifyData.length; i++) {  
+                        //匹配cat_id                  
+                        if (catData[index].cat_id === classifyData[i].cat_id) {
+                           //生成插入数据
+                           let str = '';
+                           classifyData[i].data.forEach(item => {
+                                str += `<a href="">${item.product_content}</a>`
+                           })
+                           //进行插入,移入显示
+                           let location =parseInt(index/7)
+                            shopliChild[location].innerHTML = str;
+                            shopliChild[location].style.display = 'block'
+                        }
+                    }
+                };
+                //鼠标移出导航栏
+                shopLi[index].onmouseleave = function (){
+                    let location =parseInt(index/7)
+                    shopliChild[location].style.display = 'none'
+                };
+            }
+            // 保证移入子导航栏还能显示
+            for (let i = 0; i < shopliChild.length; i++) {
+                shopliChild[i].onmouseenter = function(){
+                    shopliChild[i].style.display = 'block'
+                };
+                shopliChild[i].onmouseleave = function (){
+                    shopliChild[i].style.display = 'none'
+                };
+            }
         }
-    } else if (i >= 8 && i <= 14) {
-        shopli[i].onmouseenter = function () {
-            shopliChild[1].style.display = 'block'
-        }
-        shopli[i].onmouseleave = function () {
-            shopliChild[1].style.display = 'none'
-       }
-    }
-    else if (i == 16) {
-        shopli[i].onmouseenter = function () {
-            shopliChild[2].style.display = 'block'
-        }
-        shopli[i].onmouseleave = function () {
-            shopliChild[2].style.display = 'none'
-       }
-    }
-}
+    })
+})();
+
+//搜索
+(function(){
+    searchSpan.addEventListener('click', function() {
+        // 获取输入框的值
+        let inputValue = searchInput.value;
+    
+    });
+})();
+
+
---
+++ project/lc/js/hc_privateJS.js
@@ -0,0 +1,67 @@
+/*
+    2024/12/28 开始制作 自用封装js方法
+    作者：linmo
+*/
+
+//ajax请求
+let hc_ajax = {
+    //ajax方法，传入请求方法，数据地址，请求参数，请求成功运行的函数
+    ajax({ method, url, data = {}, success } = {}) {
+        // 创建ajax实例对象
+        let xhr = new XMLHttpRequest();
+        // 监听事件
+        xhr.onreadystatechange = function () {
+            // 验证ajax的状态
+            if (xhr.readyState == 4) {
+                //验证http状态
+                if (xhr.status >= 200 && xhr.status < 300 || xhr.status == 304) {
+                    //拿参数改JSON格式
+                    let successdata = JSON.parse(xhr.response);
+                    //把拿到的数据给到自定义的success函数
+                    success(successdata);
+                }
+            }
+        };
+
+        //处理请求参数data（post上传表单和json，get请求url编码，toLowerCase()全转小写）
+        let str;
+        if (method.toLowerCase() === 'post' && data && typeof data === 'object') {
+            // 如果是 POST 请求，并且 data 是对象
+            if (data instanceof FormData) {
+                //提交表单
+                str = data;
+            } else {
+                //提交json
+                str = JSON.stringify(data);
+            }
+        } else {
+            //get请求数据转换为 URL 编码格式
+            str = Object.entries(data).map((item) => {
+                return item.join('=');
+            }).join('&');
+        }
+
+
+        if (method.toLowerCase() == 'get') {
+            // 配置
+            xhr.open('get', url + '?' + str);
+            // 发送
+            xhr.send();
+        } else if (method.toLowerCase() == 'post') {
+            // 配置
+            xhr.open('post', url);
+            //判断请求参数
+            if (data instanceof FormData) {
+                //自动处理，无方法
+            } else if (typeof data === 'object') {
+                //以json格式发送
+                xhr.setRequestHeader('Content-Type', 'application/json');
+            } else {
+                //以 URL 编码的格式发送
+                xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+            }
+            // 发送
+            xhr.send(str);
+        }
+    }
+}
