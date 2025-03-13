dc2f86f259a9c627dfc24863572222d2337398fe
linmo <hc0708king@163.com>
2025-03-14 01:04:00 +0800

    鸿蒙购物软件完成

---
+++ project/lc/j.js
@@ -0,0 +1,20 @@
+{
+    "_id":"661e2d882ce5d24c0005a1cf",
+    "goods_id":"106171",
+    "cat_id":"0141",
+    "goods_name":"日式风吕敷丝巾-折纸 | 日剧里的小包裹 便当巾",
+    "goods_desc":"“这款设计基于日本传统折纸艺术（おりがみorigami）延续扩展了折叠与包...",
+    "goods_thumb":"https://imgs-qn.iliangcang.com/ware/goods/big/2/106/106171.jpg",
+    "brand_name":"Link",
+    "brand_thumb":"https://imgs-qn.iliangcang.com/ware/brand/176.jpg",
+    "goods_number":"1"
+    ,"star_number":"0"
+    ,"price":328
+},
+    
+{
+    "_id":"661e2d882ce5d24c00059fd6"
+    ,"goods_id":"252737"
+    ,"cat_id":"0082"
+    ,"goods_name":"便携茶具 砂岩釉分享套装 | 含茶壶茶杯茶盘等全套茶具"
+    ,"goods_desc":"1、更防烫 专利中空隔热设计，防烫效果提升40%；\n2、出水顺 两段式壶嘴结...","goods_thumb":"https://imgs-qn.iliangcang.com/ware/goods/big/2/252/252737.jpg","brand_name":"泊喜小巨蛋","brand_thumb":"https://imgs-qn.iliangcang.com/ware/brand/482.jpg","goods_number":"1","star_number":"0","price":498}
